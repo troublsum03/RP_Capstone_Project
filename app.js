d8876ce47856f9ea542c30a58054eaeee325e497
troublsum03 <ronaldpennix6918@gamil.com>
2020-01-23 23:46:04 -0600

    second commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// app
+const app = express();
+
+//db
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('DB Connected'));
+
+//routes
+app.get('/', (req, res) => {
+  res.send('hello from node updated');
+});
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
